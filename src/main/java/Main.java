934878374a9c88ce169cb6a5e8462c9ac3268625
Andrew H. Rohn <44759355+ahrohn92@users.noreply.github.com>
2022-03-05 00:07:30 -0500

    inital commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private static final int INIT_WIDTH = 800;
+    private static final int INIT_HEIGHT = 600;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Schedule App");
+        primaryStage.setWidth(INIT_WIDTH);
+        primaryStage.setHeight(INIT_HEIGHT);
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/Task.java
@@ -0,0 +1,44 @@
+import java.util.Date;
+import java.util.List;
+
+public class Task {
+
+    private String desc;
+    private List<Task> subTasks;
+    private boolean isComplete = false;
+    private Date date;
+
+
+    public Task(String desc) {
+
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setComplete(boolean complete) {
+        isComplete = complete;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
